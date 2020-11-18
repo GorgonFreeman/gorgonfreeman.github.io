c21ea00465ee500d7ff383638005c5af37f27335
John Martin <john@createur.com.au>
2020-11-18 23:44:28 +1100

    gulp sass initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
 // Static server
 gulp.task('start', function() {
@@ -8,4 +9,12 @@ gulp.task('start', function() {
       baseDir: "./"
     }
   });
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("src/styles/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("dist/styles"))
+    .pipe(browserSync.stream());
 });
