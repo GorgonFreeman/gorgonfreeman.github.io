0c9340ae35bcaa4b63d500f4e9228ebe8fa51905
John Martin <john@createur.com.au>
2020-11-19 22:36:13 +1100

    typography initial

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,24 @@ const sass = require('gulp-sass');
 gulp.task('start', function() {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: './'
     }
   });
 
-  gulp.watch("src/styles/*.scss", gulp.parallel(['sass']));
-  gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch('src/styles/*.scss', gulp.parallel(['sass']));
+  gulp.watch('src/assets/*', gulp.parallel(['assets']));
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  return gulp.src("src/styles/*.scss")
+  return gulp.src('src/styles/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest("dist/styles"))
+    .pipe(gulp.dest('dist/styles'))
     .pipe(browserSync.stream());
+});
+
+gulp.task('assets', function() {
+  return gulp.src('src/assets/*')
+    .pipe(gulp.dest('dist/assets'));
 });
