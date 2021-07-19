59242d67910509f0fa39bbfbc55296910ae19c17
John Martin <john@createur.com.au>
2021-07-20 08:00:20 +1000

    working swiper implementation

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -1,1 +1,15 @@
-console.log('Hey');
+// core version + navigation:
+import SwiperCore, { Navigation } from 'swiper/core';
+
+// configure Swiper to use modules
+SwiperCore.use([Navigation]);
+
+// import Swiper styles
+// import 'swiper/swiper-bundle.css';
+
+const swiper = new SwiperCore(".mySwiper", {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
